d7f662d50e461f73904270f2260db885b981c363
1poundSOCKS <mathew.coburn@gmail.com>
2025-08-10 08:48:43 +0100

    reads a record from postgres

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 type Person struct {
@@ -12,7 +15,40 @@ type Person struct {
 	Age  int    `json:"age"`
 }
 
+type User struct {
+	transaction_time string
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
+
+	// "db_connection": "host=localhost port=5432 dbname=mydatabase user=myuser password=mypassword",
+
+	connStr := "postgres://myuser:mypassword@localhost:5432/mydatabase?sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Error opening connection: ", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Cannot connect to database: ", err)
+	}
+
+	var version string
+	err = db.QueryRow("SELECT version()").Scan(&version)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("PostgreSQL version:", version)
+
+	var u User
+	err = db.QueryRow(`SELECT transaction_time FROM jobs`).Scan(&u.transaction_time)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("transaction time:", u.transaction_time)
 
 	p := Person{Name: "Alice", Age: 30}
 
